f9eb716daaafe671247eb121f13fa0adefa912c7
Matthias Schneider <ms@wck.biz>
2018-01-16 13:52:55 +0100

    added a dummy CA to trust other container with selfsinged certs if TLS_FQDN is used

--- root/etc/cont-init.d/102-gen-tls-cert.sh
+++ root/etc/cont-init.d/102-gen-tls-cert.sh
@@ -2,6 +2,8 @@
 
 if [ ! -z $TLS_FQDN ] ;
 then
-    /usr/bin/generate_cert -days 3600 -host $TLS_FQDN -cert ${APP_HOME}/.minio/certs/public.crt -key ${APP_HOME}/.minio/certs/private.key
+    mkdir -p ${APP_HOME}/.minio/certs/CAs
+    cp -a /etc/ca/ca.crt ${APP_HOME}/.minio/certs/CAs
+    /usr/bin/generate_cert -ca /etc/ca/ca.crt -ca-key /etc/ca/ca.key -days 3600 -org "MinioServer" -host "${TLS_FQDN}" -cert ${APP_HOME}/.minio/certs/public.crt -key ${APP_HOME}/.minio/certs/private.key
     chown ${USER}:${GROUP} ${APP_HOME}/.minio/certs/public.crt ${APP_HOME}/.minio/certs/private.key
 fi

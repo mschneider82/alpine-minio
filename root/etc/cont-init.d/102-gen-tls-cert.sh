2521f46fbf14090ddcccd0d8597208efffe5f772
Matthias Schneider <ms@wck.biz>
2018-01-16 13:58:42 +0100

    added auto ip discovery for TLS Cert

--- root/etc/cont-init.d/102-gen-tls-cert.sh
+++ root/etc/cont-init.d/102-gen-tls-cert.sh
@@ -4,6 +4,7 @@ if [ ! -z $TLS_FQDN ] ;
 then
     mkdir -p ${APP_HOME}/.minio/certs/CAs
     cp -a /etc/ca/ca.crt ${APP_HOME}/.minio/certs/CAs
-    /usr/bin/generate_cert -ca /etc/ca/ca.crt -ca-key /etc/ca/ca.key -days 3600 -org "MinioServer" -host "${TLS_FQDN}" -cert ${APP_HOME}/.minio/certs/public.crt -key ${APP_HOME}/.minio/certs/private.key
+    MY_IP=$(ip route get 8.8.8.8 | awk '{print $7}')
+    /usr/bin/generate_cert -ca /etc/ca/ca.crt -ca-key /etc/ca/ca.key -days 3600 -org "MinioServer" -host "${TLS_FQDN},${MY_IP}" -cert ${APP_HOME}/.minio/certs/public.crt -key ${APP_HOME}/.minio/certs/private.key
     chown ${USER}:${GROUP} ${APP_HOME}/.minio/certs/public.crt ${APP_HOME}/.minio/certs/private.key
 fi
